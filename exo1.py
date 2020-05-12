4982aa453f4cc136caa40f0d6a9ec778f755464c
Youneselakkaoui <younes.elakkaoui@gmail.com>
2020-05-12 03:10:25 +0200

    Update exo1.py

--- exo1.py
+++ exo1.py
@@ -2,3 +2,4 @@ print("Hello This is the first Python Exercice !")
 print("This is the second line in the projet !")
 dic = {"Casablanca":1000, "Fes":200, "Marrakech":300, "Oujda":100}
 for k,v in dic:
+  # this is a comment
