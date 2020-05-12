9f68f0554365108203e4906deb588c74030ce575
Younes <younes.elakkaoui@gmail.com>
2020-05-12 03:12:29 +0200

    add dictionnary to the source code

--- exo1.py
+++ exo1.py
@@ -1,5 +1,5 @@
 print("Hello This is the first Python Exercice !")
 print("This is the second line in the projet !")
 dic = {"Casablanca":1000, "Fes":200, "Marrakech":300, "Oujda":100}
-for k,v in dic:
-  print(k,v)
+for k in dic:
+  print(k,":", dic[k])
